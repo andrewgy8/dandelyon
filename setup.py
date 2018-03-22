a1b2ac4e0da848dca9c2a137f45ccfaf8bb499f7
Andrew Graham-Yooll <andrewgy8@gmail.com>
2018-03-22 22:10:37 +0100

    Added deployment step to circle ci

--- setup.py
+++ setup.py
@@ -2,8 +2,26 @@
 
 """The setup.py file for Python Fire."""
 
+import os
+import sys
 from setuptools import setup, find_packages
+from setuptools.command import install
 
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that 
+    the git tag matches our version"""
+
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
 
 LONG_DESCRIPTION = """
 Dandelyon is a library for handling deprecation events that occur
@@ -39,6 +57,11 @@ setup(name='Dandelyon',
       packages=find_packages(),
       test_suite='tests.test_deprecate',
       install_requires=DEPENDENCIES,
-      tests_require=TEST_DEPENDENCIES
+      tests_require=TEST_DEPENDENCIES,
+      keywords='deprecate deprecation decorator',
+      python_requires='>=3',
+      cmdclass={
+          'verify': VerifyVersionCommand,
+          }
       )
 
